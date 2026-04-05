1b7380eaf7c1b2fbc2f837ecca831bcdd1bfe3fc
cgoecke <92066008+cgoecke@users.noreply.github.com>
2026-04-05 19:05:43 -0400

    Update activations.py
    forward/backward defs for ReLU and Softmax activation.

---
+++ src/activations.py
@@ -0,0 +1,23 @@
+#Activations code file
+import numpy as np
+
+#ReLU activation
+class ReLU:
+  def forward(self,input):
+    self.input = input
+    return np.maximum(0, input)
+
+  def backeward(self, grad_output):
+    grad = grad_output.copy()
+    grad[self.input<=0] = 0
+    return grad
+
+#Softmax activation
+class Softmax:
+  def forward(self,input):
+    exp_values = np.exp(input-np.max(input, axis=1, keepdims=True))
+    self.output=exp_values/np.sum(exp_values, axis=1, keepdims=True)
+    return self.output
+
+  def backward(self, grad_output):
+    return grad_output #handled with cross entropy in loss.py
