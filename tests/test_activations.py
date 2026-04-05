64db6d5fa5121c8236a0265f844cee020e63a01f
cgoecke <92066008+cgoecke@users.noreply.github.com>
2026-04-05 19:25:09 -0400

    Update test_activations.py
    tests  ReLU forward and backward. tests that the softmax output sums to 1.

--- tests/test_activations.py
+++ tests/test_activations.py
@@ -1,1 +1,25 @@
 #Tests ReLU and Softmax
+#Can run with pytest in a local terminal
+
+import numpy and np
+from activations import ReLU, Softmax
+
+def test_relu_forward():
+  relu = ReLU()
+  x = np.array([[-1, 0, 2]])
+  out = relu.forward(x)
+  assert np.array_equal(out, np.array([[0, 0, 2]])
+
+def test_relu_backward():
+  relu = ReLU()
+  x = np.array([[-1, 0, 2]])
+  relu.forward(x)
+  grad_output = np.array([[1, 1, 1]])
+  grad_input = relu.backward(grad_output)
+  assert np.array_equal(grad_input, np.array([[0, 0, 1]]))
+
+def test_softmax_output_sums_to_one():
+  softmax = Softmax()
+  x = np.array([[1, 2, 3]])
+  out = softmax.forward(x)
+  assert np.allclose(np.sum(out, axis=1), 1.0)
